872c30fd989eb4dff1e2ca48986d2a54d4155a41
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-08-19 12:54:25 +0000

    8/19-bookers2[edit]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.new
+    @book_detail = Book.find(params[:id])
+  end
+  
+  def edit
+    @book_detail = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    @book.update
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book_detail = Book.find(params[:id])
+    @book_detail.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
+end
