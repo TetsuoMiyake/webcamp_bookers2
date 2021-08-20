b80b7eb91ba072df50a7a00a561c4e84f876d3ef
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-08-20 13:53:51 +0000

    8/20-bookers2[edit]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,15 +20,15 @@ class BooksController < ApplicationController
     @book = Book.new
     @book_detail = Book.find(params[:id])
   end
-  
+
   def edit
     @book_detail = Book.find(params[:id])
   end
-  
+
   def update
-    @book = Book.find(params[:id])
-    @book.update
-    redirect_to book_path(@book.id)
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
