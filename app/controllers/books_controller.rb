e2037869fd53dc20e45f62146d5cab839f484199
TetsuoMiyake <tmky1919mnstwebcamp@gmail.com>
2021-08-22 22:15:22 +0000

    8/23-bookers2-book-error-finish]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,8 +7,12 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -27,9 +31,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book_detail = Book.find(params[:id])
+    if @book_detail.update(book_params)
+      redirect_to book_path(@book_detail.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
